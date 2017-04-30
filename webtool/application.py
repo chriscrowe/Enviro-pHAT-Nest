2408e137c1e14d2d42103c78e7f1f7159d70f029
skandocious <skandocious@gmail.com>
2017-04-30 13:01:42 -0700

    Getting started on the web server
    Only just setup the scaffolding... Thinking about having the temp reader
    running as a separate process all the time and using xmlrpclib or rpyc
    to send commands and queries to it from the web tool. Not sure yet.

---
+++ webtool/application.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/set-enabled/<enabled>')
+def set_enabled(enabled):
+    return render_template('set_enabled.html', enabled=enabled)
+
+@app.route('/activate')
+def activate():
+    return set_enabled(True)
+
+@app.route('/deactivate')
+def activate():
+    return set_enabled(False)
+
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
